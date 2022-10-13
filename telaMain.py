86eb86cc6c340ad41b7b33b1694902ddd2a4ff44
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 14:57:52 -0300

    Iniciando o arquivo apagando o proximo pycache

--- telaMain.py
+++ telaMain.py
@@ -1,2 +1,3 @@
 from tkinter import DoubleVar, StringVar, TclError, Tk, Label, Button, Entry, Text, PhotoImage, messagebox
 from cript import setSave, setCriptografador, setDescriptografador, ArquivoINTL
+ArquivoINTL()
