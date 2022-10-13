6f3b470bc52873907796f2ff276630a89e60a227
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 13:47:04 -0300

    Gerando o token-criptografia aleatório

--- cript.py
+++ cript.py
@@ -11,4 +11,13 @@ def ArquivoINTL():  # apagando a pasta __pycache__
 
 ArquivoINTL()
 
+letters = [' ', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G',
+           'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N',
+           'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U',
+           'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
+complexidade = 10
 
+
+def GerarToken(complexidade=complexidade, caracter=ascii_lowercase + ascii_uppercase + digits):
+    token = ''.join(choice(caracter)for _ in range(complexidade))
+    return token
