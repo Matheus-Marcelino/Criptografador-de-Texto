37ddeaf2ed0e5e8644019859cca10e5a15666db3
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 13:44:35 -0300

    Excluindo a pasta pycache que o python cria

--- cript.py
+++ cript.py
@@ -2,3 +2,13 @@ from string import ascii_lowercase, ascii_uppercase, punctuation, digits
 from os import mkdir, path
 from shutil import rmtree
 from random import choice
+
+
+def ArquivoINTL():  # apagando a pasta __pycache__
+    if path.exists('__pycache__'):
+        rmtree('__pycache__')
+
+
+ArquivoINTL()
+
+
