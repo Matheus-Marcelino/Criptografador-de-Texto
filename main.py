f11a1eb9fc538fb6ef35048436cb87d59dabfdbb
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 15:27:21 -0300

    Criando a funcao da animacao e iniciando com o organizador

--- main.py
+++ main.py
@@ -2,3 +2,23 @@ from tkinter import TclError, Tk, Label, PhotoImage, messagebox
 from cript import setOrganizador
 from telaMain import main
 from time import sleep
+
+
+def animacao():
+    try:
+        for _ in range(5):  # quanto vai demorar
+            for j in range(16):  # quantidade de quadrados
+                # colorindo de verde
+                Label(WINDOW_LOADING, bg='#00c900',
+                      width=2, height=1).place(x=(j+4)*22, y=100)
+                sleep(0.06)
+                WINDOW_LOADING.update_idletasks()
+
+                # colorindo de preto
+                Label(WINDOW_LOADING, bg='#1F2732',
+                      width=2, height=1).place(x=(j+4)*22, y=100)
+        ORGANIZADOR = setOrganizador()
+        WINDOW_LOADING.destroy()
+        return ORGANIZADOR
+    except TclError:
+        pass
